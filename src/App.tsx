9a2eb648eae911054ae157a1985b746485c37b15
Jusung Hwang (nabigraphics) <hbh2692@naver.com>
2024-05-13 17:25:59 +0900

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+import "./styles/global.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div>hello world!</div>;
+}
+
+export default App;
